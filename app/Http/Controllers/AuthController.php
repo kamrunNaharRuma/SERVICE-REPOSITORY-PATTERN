6254de22a184ab9498204807fba3ebec16532ed3
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-10 20:01:00 +0600

    modified the keys

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\FileUploadPath\userTypeConstant;
+use App\Constants\UserTypeConstant;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -13,10 +13,10 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         return User::create([
-            'user' => $request->input('name'),
+            'name' => $request->input('name'),
             'email' => $request->input('email'),
             'password' => Hash::make($request->input('password')),
-            'user_type_id' => userTypeConstant::USER_TYPE_ID_FOR_USERS,
+            'user_type_id' => UserTypeConstant::USER_TYPE_ID_FOR_USERS,
         ]);
     }
 }
