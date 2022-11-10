b1f98f73d5cc25a8bf2ba3e85d03ab41ca8ec0bf
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-10 19:37:11 +0600

    basic register function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     //
+    public function register(Request $request)
+    {
+        return User::create([
+            'user' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+    }
 }
