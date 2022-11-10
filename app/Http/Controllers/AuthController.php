db5262ace1468dcec6b179e0feba41ecb697b44a
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-11 00:17:42 +0600

    setting up COOKIES

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Constants\UserTypeConstant;
+use App\Http\Responses\ApiResponse;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -23,5 +26,23 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return Response(
+                [
+                    "message" => "Invalid credentials!"
+                ],
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+        $user = Auth::user();
+        $user = User::find($user['id']);
+        $token = $user->createToken('token')->plainTextToken;
+        $cookie = cookie('jwt', $token, 60 * 24);
+        return response(["message" => "Successfully logged in", "token" => $token])->withCookie($cookie);
+    }
+
+    public function user()
+    {
+        return "authorized";
     }
 }
