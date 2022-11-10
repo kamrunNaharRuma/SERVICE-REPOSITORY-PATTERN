54af895944f2f8a4306b55e037a1764530f9e00d
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-10 19:36:46 +0600

    Modified existing user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,16 +29,6 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'password'
     ];
 }
