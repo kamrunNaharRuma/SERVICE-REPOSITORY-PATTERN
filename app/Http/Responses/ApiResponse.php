b929769406c6526f60eedbdad18a71af3b53fec9
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-11 00:16:44 +0600

    API respomse class modified

--- app/Http/Responses/ApiResponse.php
+++ app/Http/Responses/ApiResponse.php
@@ -2,23 +2,25 @@
 
 namespace App\Http\Responses;
 
-class SuccessResponse
+class ApiResponse
 {
     private string $message;
     private $data;
     private $statusCode;
+    private $isSuccessful;
 
-    public function __construct(string $message, $data = null, $statusCode)
+    public function __construct(string $message, $data = null, $statusCode, $isSuccessful)
     {
         $this->data         = $data;
         $this->message      = $message;
         $this->statusCode   = $statusCode;
+        $this->isSuccessful   = $isSuccessful;
     }
 
     public function getPayload(): array
     {
         return [
-            'success'       => true,
+            'success'       => $this->isSuccessful,
             'status_code'   => $this->statusCode,
             'message'       => $this->message,
             'data'          => $this->data
