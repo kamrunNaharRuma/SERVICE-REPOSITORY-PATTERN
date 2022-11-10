36c5ccc2b9522b0f4614069c4b75533f32f84e6b
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-10 19:49:48 +0600

    registering for general user by default

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\FileUploadPath\userTypeConstant;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -14,7 +15,8 @@ class AuthController extends Controller
         return User::create([
             'user' => $request->input('name'),
             'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password'))
+            'password' => Hash::make($request->input('password')),
+            'user_type_id' => userTypeConstant::USER_TYPE_ID_FOR_USERS,
         ]);
     }
 }
