8c55bccef7a70b0e0cea94a07c93e91ac5253338
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-11 18:10:04 +0600

    validation added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,24 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Constants\UserTypeConstant;
+use App\Http\Responses\ApiException;
 use App\Http\Responses\ApiResponse;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class AuthController extends Controller
 {
     //
     public function register(Request $request)
     {
-        return User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password')),
-            'user_type_id' => UserTypeConstant::USER_TYPE_ID_FOR_USERS,
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email:rfc,dns',
+            'password' => 'required',
         ]);
+        try {
+            $user =  User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password')),
+                'user_type_id' => UserTypeConstant::USER_TYPE_ID_FOR_USERS,
+            ]);
+            return (new ApiResponse('Registered successfully', $user, Response::HTTP_CREATED, true))->getPayload();
+        } catch (\Exception $e) {
+            throw new ApiException($e->getMessage());
+        }
     }
 
 
@@ -38,11 +50,6 @@ class AuthController extends Controller
         $user = User::find($user['id']);
         $token = $user->createToken('token')->plainTextToken;
         $cookie = cookie('jwt', $token, 60 * 1);
-        return response(["message" => "Successfully logged in", "token" => $token])->withCookie($cookie);
-    }
-
-    public function user()
-    {
-        return "authorized";
+        return response(["message" => "Successfully logged in"])->withCookie($cookie);
     }
 }
