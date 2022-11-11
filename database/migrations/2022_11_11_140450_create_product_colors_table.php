ec2c9246ddaa70818b1446b52f7d3ca792a3d5ba
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-11 20:08:26 +0600

    migrations for all attributes related to product

---
+++ database/migrations/2022_11_11_140450_create_product_colors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductColorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_colors', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('product_id');
+            $table->foreignId('color_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_colors');
+    }
+}
