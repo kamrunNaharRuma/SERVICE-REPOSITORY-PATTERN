6ce96698e69d7f376f0c6fd2c1fd5cffcf2c156a
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-11 17:31:18 +0600

    category service interface

---
+++ app/Services/CategoryServiceInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Category;
+
+interface CategoryServiceInterface
+{
+
+    public function all();
+
+    public function store(array $data);
+
+    public function delete($id);
+
+    public function edit($id);
+
+    public function update(array $data, Category $category);
+}
