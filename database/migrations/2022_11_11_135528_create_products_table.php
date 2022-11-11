e155cd5a93e5a96859a00cfa928351bd44e7327d
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-11 20:09:40 +0600

    product table

--- database/migrations/2022_11_11_135528_create_products_table.php
+++ database/migrations/2022_11_11_135528_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('slug')->unique()->after('name');
+            // $table->string('slug')->unique()->after('name');
             $table->integer('price');
             $table->timestamps();
         });
