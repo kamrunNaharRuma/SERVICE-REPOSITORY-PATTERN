cfb10649088710cf9f10c7fc1791474f05905a1a
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-11 17:34:35 +0600

    categories resources with middleware

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('user', [AuthController::class, 'user']);
+    Route::resource('categories', CategoryController::class);
 });
