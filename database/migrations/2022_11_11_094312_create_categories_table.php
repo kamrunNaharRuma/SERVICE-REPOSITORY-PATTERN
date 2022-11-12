a53ef9f0da108ccd56e9c756af685c8c3bfdcba8
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-12 17:48:08 +0600

    primary category table added

--- database/migrations/2022_11_11_094312_create_categories_table.php
+++ database/migrations/2022_11_11_094312_create_categories_table.php
@@ -16,6 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('type')->unique();
+            $table->foreignId('primary_category_id')->nullable();
             $table->timestamps();
         });
     }
