c6c3201f39a640418de0be15ee7a9d031c93e24b
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-12 13:09:00 +0600

    Product Store Request added

---
+++ app/Http/Requests/ProductStoreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+
+class ProductStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Gate::allows('store-update-delete-product', Auth::user());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'slug' => 'required',
+            'price' => 'required|integer',
+            'color_id' => 'array',
+            'size_id' => 'array',
+            'category_id' => 'required|array',
+            'image' => 'required|array'
+        ];
+    }
+}
