37326f30b12fa331fa5b2069f90dff4e4e3a5917
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-12 10:36:28 +0600

    product repository

---
+++ app/Repositories/ProductRepositoryImplementation.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product;
+use App\Repositories\ProductRepositoryInterface;
+
+class ProductRepositoryImplementation implements ProductRepositoryInterface
+{
+
+    private $product;/*  */
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    public function all()
+    {
+        return $this->product->get();
+    }
+
+
+    public function store(array $data)
+    {
+        return $this->product->create([
+            "name" => $data['name'],
+            "slug" => $data['slug'],
+            "price" => $data['price'],
+            "created_at" => now()
+        ]);
+    }
+
+    public function delete($id)
+    {
+        return $this->product->delete($id);
+    }
+
+    public function edit($id)
+    {
+        return $this->product->edit($id);
+    }
+
+    public function update(array $data, $product)
+    {
+        return $this->product->where('id', $product->id)->update($data);
+    }
+}
---
+++ app/Repositories/ProductRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories;
+
+interface ProductRepositoryInterface
+{
+    public function all();
+
+    public function store(array $data);
+
+    public function delete($id);
+
+    public function edit($id);
+
+    public function update(array $data, $id);
+}
