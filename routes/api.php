a116c38e5234dd59a926a0c7db475aa0b3a72a5d
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-12 10:37:14 +0600

    Product repository

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,4 +12,5 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
 });
