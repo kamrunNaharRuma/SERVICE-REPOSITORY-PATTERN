73faaef1748110c92049d20307c89b239c236968
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-12 13:11:56 +0600

    Path added in migration

--- database/migrations/2022_11_11_135715_create_product_images_table.php
+++ database/migrations/2022_11_11_135715_create_product_images_table.php
@@ -16,6 +16,7 @@ class CreateProductImagesTable extends Migration
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id');
+            $table->string('path');
             $table->timestamps();
         });
     }
