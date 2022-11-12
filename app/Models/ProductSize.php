dae70401464a37a827427fd707f6b22e0e25ec7b
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-11-12 10:34:52 +0600

    ProductSize model

---
+++ app/Models/ProductSize.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSize extends Model
+{
+    use HasFactory;
+
+    protected $table = "product_sizes";
+}
